c6ccb475cdddb2f8e0eb9be2ac0dd41d03aef03b
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2021-07-28 09:37:47 -0300

    microfrontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'lojax';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    this.addMicroFrontEndCarrinho();
+  }
+
+  addMicroFrontEndCarrinho() {
+    const script = document.createElement('script');
+    script.src = 'http://localhost:8080/main.js';
+    document.body.appendChild(script);
+  }
+
+  escutaComprou() {
+    const carrinho = document.querySelector('carrinho-app');
+    carrinho?.addEventListener('comprado', () => {
+      console.log('comprou');
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,13 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+
+  }
+}
